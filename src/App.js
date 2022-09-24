56ed969ac2e01fc7e448ce65895607365d5d6efb
Andrii Katsalap <andriikatsalap@gmail.com>
2022-09-24 23:51:46 +0300

    doctor-request

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import ChooseFile from './Components/ChooseFileComponent/ChooseFile';
 import './App.css';
 import Panel from './Components/Panel/Panel';
 import CalendarItem from './View/CalendarItem/CalendarItem';
+import { Route, Routes } from "react-router-dom";
+import DoctorRequest from './View/DoctorRequest/DoctorRequest';
+import Notifications from './View/Notification/Notification';
 
 function App() {
-
   return (
     <div className="App">
       <Panel />
-
-      <CalendarItem />
-
+      <Routes>
+        <Route path="/" element={<CalendarItem />} />
+          <Route path="/doctorRequest" element={<DoctorRequest/>}/>
+          <Route path="/notification" element={<Notifications/>}/>
+          <Route/>
+      </Routes>
     </div>
   );
 

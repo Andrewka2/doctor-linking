17cb42586ebd890145ae98903b5f4d05963e5dc8
luckylu03 <44835935+luckylu03@users.noreply.github.com>
2022-12-12 12:03:16 +0200

    call abt scroll and stick

--- src/Components/Schedule/Schedule.jsx
+++ src/Components/Schedule/Schedule.jsx
@@ -6,7 +6,7 @@ import arrow from '../../assets/arrow.png';
 import { useSelector } from 'react-redux'
 import ChooseFile from "../../Components/ChooseFileComponent/ChooseFile";
 import { useDispatch } from 'react-redux'
-import { updateStatus } from "../../root/actions/scheduleActions";
+import { updateStatus, updateDate } from "../../root/actions/scheduleActions";
 import { useEffect } from "react";
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
@@ -28,89 +28,102 @@ let statuses = [{'В': 'Вихідний'}, {'7': '7 годин'}, {'12': '12 г
 
 export default function Schedule() {
 
-    const scheduleData = useSelector((state) => state.schedule.rawData)
+    const scheduleData = useSelector((state) => state.schedule)
     const dispatch = useDispatch()
 
     let [selectedStatus, setSelectedStatus] = useState('')
     let [value, setValue] = useState(null)
-    let [data, setNewData] = useState(null)
-    // let [curDate, setDate] = useState(new Date())
-    let [curDate, setDate] = useState(new Date('2022/10/01'))
     let [visibleData, setVisibleData] = useState(null)
     let [visibleRows, setVisibleRows] = useState(null)
-
-    let weekStart = getWeekStart(curDate).getDate()
-    Array.from({length: 8}, (_, i) => {
-        console.log('day');
-        console.log(i + weekStart - 1);
-        return i + weekStart - 1})
-    
-    let [calendarNumberOfDate, setCalendarNumberOfDate] = useState([])
-    let [calendarData, setCalendarData] = useState(null)
+    let [isModalOpen, setModalOpen] = useState(false)
+    let [rowClicked, setRowClicked] = useState(false)
     
     useEffect(() => {
-        setNewData(scheduleData)
-        if(visibleData == null){
-            let tempData = generateCalendarData(scheduleData, getMonthDays(curDate))
-            setVisibleData(tempData.displayData)
-            setVisibleRows(tempData.visibleRows)
-        }
-        else{
-            defineData(scheduleData)
-        }
+        console.log(scheduleData.chosenDate);
+        defineData(scheduleData.rawData)
     }, [scheduleData])
 
-    function defineData(input = data, selectedOption = value) {
+    function defineData(input, selectedOption = value) {
         let tempInput
         if(selectedOption !== null && selectedOption.value === options[0].value){
-            tempInput = generateDayData(input, curDate)
-            updateVisibleData(tempInput.displayData, tempInput.visibleRows)
+            tempInput = generateCalendarData(input, getDay(scheduleData.chosenDate))
+            visibleDataHandler(tempInput.displayData)
+            visibleRowsHandler(tempInput.visibleRows)
         }
         if(selectedOption !== null && selectedOption.value === options[1].value){
-            tempInput = generateWeekData(input, curDate)
-            updateVisibleData(tempInput.displayData, tempInput.visibleRows)
+            tempInput = generateCalendarData(input, getWeekDays(scheduleData.chosenDate))
+            visibleDataHandler(tempInput.displayData)
+            visibleRowsHandler(tempInput.visibleRows)
         }
         else if (selectedOption == null || selectedOption.value === options[2].value){
-            tempInput = generateCalendarData(input, getMonthDays(curDate))
-            updateVisibleData(tempInput.displayData, tempInput.visibleRows)
+            tempInput = generateCalendarData(input, getMonthDays(scheduleData.chosenDate))
+            visibleDataHandler(tempInput.displayData)
+            visibleRowsHandler(tempInput.visibleRows)
         }
     }
 
-    function updateVisibleData(data, rows) {
-        setVisibleRows(rows)
+    function visibleDataHandler(data){
         setVisibleData(data)
+    }
+
+    function visibleRowsHandler(rows){
+        setVisibleRows(rows)
     }
 
     function handleChange(selectedOption) {
         setValue(selectedOption)
-        defineData(data, selectedOption)
+        defineData(scheduleData.rawData, selectedOption)
     };
+
+    function switchModal(){
+        setModalOpen(!isModalOpen)
+    }
+
+    function saveStatus(data){
+        console.log(data.status);
+        let newStatus = {}
+        newStatus[data.status] = data.fullStatusDesc
+        statuses.push(newStatus)
+        switchModal()
+    }
 
     function selectStatus(selectedStatus) {
         setSelectedStatus(selectedStatus)
     };
-
-    function calendarDataHandler(data){
-        setCalendarData(data)
-    }
-    
-    function handlerCalendarNumberOfDate(data){
-        setCalendarNumberOfDate(data)
-    }
 
     function saveValue(calRow, i, selectedStatus){
         console.log(visibleRows[calRow].row + ' ' + i + ' ' + selectedStatus);
         dispatch(updateStatus(visibleRows[calRow].row, i, selectedStatus))
     }
 
-    function prevMonth() {
-        setDate(new Date(curDate.setMonth(curDate.getMonth()-1)))
-        dispatch(updateStatus(1, 1, 'qwe'))
+    function prevRange() {
+        let curDate = new Date(scheduleData.chosenDate)
+        let newDate
+        if(value !== null && value.value === options[0].value){
+            newDate = curDate.getTime() - getDayTime()
+        }
+        if(value !== null && value.value === options[1].value){
+            newDate = curDate.getTime() - getDayTime()*7
+        }
+        else if (value == null || value.value === options[2].value){
+            newDate = curDate.setMonth(curDate.getMonth() - 1)
+        }
+        dispatch(updateDate(new Date(newDate)))
     }
 
-    function nextMonth() {
-        setDate(new Date(curDate.setMonth(curDate.getMonth()+1)))
-        dispatch(updateStatus(1, 1, 'qwe'))
+    function nextRange() {
+        let curDate = new Date(scheduleData.chosenDate)
+        let newDate
+        if(value !== null && value.value === options[0].value){
+            newDate = curDate.getTime() + getDayTime()
+        }
+        if(value !== null && value.value === options[1].value){
+            newDate = curDate.getTime() + getDayTime()*7
+        }
+        else if (value == null || value.value === options[2].value){
+            newDate = curDate.setMonth(curDate.getMonth() + 1)
+        }
+        dispatch(updateDate(new Date(newDate)))
     }
 
     const {
@@ -124,16 +137,15 @@ export default function Schedule() {
             return (
                 <div className={value === null || value.value === options[2].value ? classes.calendarUserRow : classes.calendarUserRowWeek}>
                     {Object.values(row).map((item, i) => {
-                        let dateToUpdate = new Date(curDate.getFullYear(), curDate.getMonth(), Object.keys(row)[i]).getDate()
-                        let dayOfWeek = new Date(curDate.getFullYear(), curDate.getMonth(), Object.keys(row)[i]).getDay()
+                        let date = new Date(inputData[0][i])
                         return (
                             <div
                             className={calRow === 0 ? [classes.calDateDivSpec, classes.calHeader].join(' ')
-                            :([0, 6].includes(dayOfWeek) && i !== 0) ? classes.calDateDivSpec : classes.calDateDiv}
-                            onClick={() => calRow !== 0 && i !== 0 ? saveValue(calRow, dateToUpdate, selectedStatus) : null}
+                            :([0, 6].includes(date.getDay()) && i !== 0) ? classes.calDateDivSpec : classes.calDateDiv}
+                            onClick={() => calRow !== 0 && i !== 0 ? saveValue(calRow, date.toLocaleDateString(), selectedStatus) : null}
                             >
-                                <p className={classes.calDate}>{item}</p>
-                                {(calRow === 0  && i !== 0) ? <p>{shortWeekDays[new Date(curDate.getFullYear(), curDate.getMonth(), item).getDay()]}</p> : null}
+                                {(calRow === 0  && i !== 0) ? <p className={classes.calDate}>{date.getDate()}</p> : <p className={classes.calDate}>{item}</p>}
+                                {(calRow === 0  && i !== 0) ? <p>{shortWeekDays[date.getDay()]}</p> : null}
                             </div>
                         )
                     })}
@@ -145,13 +157,13 @@ export default function Schedule() {
     return (
         <div className={classes.Body}>
             <div className={classes.itemDate}>
-                <h1>{monthNames[curDate.getMonth()] + ' ' + curDate.getFullYear()}</h1>
+                <h1>{monthNames[new Date(scheduleData.chosenDate).getMonth()] + ' ' + new Date(scheduleData.chosenDate).getFullYear()}</h1>
             </div>
             <div className={classes.ScheduleHeader}>
                 <div className={classes.rangeDiv}>
                     <h2 className={classes.headerHeading}>Період:</h2>
                     <div className={classes.rangeLeft}>
-                        <button onClick={prevMonth}><img src={arrow} alt="arrow" /></button>
+                        <button onClick={prevRange}><img src={arrow} alt="arrow" /></button>
                     </div>
                     <div className={classes.rangeSelect}>
                         <Select
@@ -161,7 +173,7 @@ export default function Schedule() {
                         />
                     </div>
                     <div className={classes.rangeRight}>
-                        <button  onClick={nextMonth}><img src={arrow} alt="arrow" /></button>
+                        <button  onClick={nextRange}><img src={arrow} alt="arrow" /></button>
                     </div>
                 </div>
                 <div className={classes.statusBody}>
@@ -175,18 +187,40 @@ export default function Schedule() {
                                     <p className={classes.status}>{Object.keys(item)[0]}</p>
                                 </div>
                             )
-                        })
-
+                            })
                         }
-                        <div className={classes.defaultStatusDiv} title="Додати новий статус">
+                        <div onClick={switchModal} className={classes.defaultStatusDiv} title="Додати новий статус">
                             <p className={classes.defaultStatus}>+</p>
                         </div>
                     </div>
+                    {isModalOpen ?
+                    <div className={classes.modalDiv}>
+                        <div className={classes.modalBackground}
+                            onClick={switchModal}
+                            >
+                        </div>
+                        <div className={classes.statusAddForm}>
+                            <form onSubmit={handleSubmit(data => saveStatus(data))}>
+                                <div className={classes.fieldCont}>
+                                    <div className={classes.fieldItem}>
+                                        <input placeholder="Абревіатура статусу" type="text" {...register('status')} />
+                                    </div>
+                                    <div className={classes.code}>
+                                        <input placeholder="Опис статусу" type="text" {...register('fullStatusDesc')} />
+                                    </div>
+                                </div>
+                                <div className={classes.send}>
+                                    <input type="submit" value={'Зберегти'}/>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    : null}
                 </div>
                 <div className={classes.headerImportDiv}>
                     <h2 className={classes.headerHeading}>Імпортувати файл:</h2>
                     <div className={classes.headerChooseFile}>
-                        <ChooseFile calendarDataHandler={calendarDataHandler} handlerCalendarNumberOfDate={handlerCalendarNumberOfDate}/>
+                        <ChooseFile calendarDataHandler={visibleDataHandler} handlerCalendarNumberOfDate={visibleRowsHandler}/>
                     </div>
                 </div>
             </div>
@@ -199,75 +233,52 @@ export default function Schedule() {
     )
 }
 
-function getWeekStart(date){
-    let dayOfWeek = date.getDay() - 1
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate() - dayOfWeek);
+function getDayTime() {
+    return 3_600_000*24
+}
+
+function getDay(date){
+    return ['', new Date(date).getTime()]
+}
+
+function getWeekDays(date){
+    let res = []
+    let dayOfWeek = new Date(date).getDay() - 1
+    let weekStart = date - getDayTime()*dayOfWeek
+    for (let i = 0; i <= 6; i++){
+        res.push(weekStart + getDayTime()*i)
+    }
+    res.unshift('')
+    return res
 }
 
 function getMonthDays(date) {
-    return new Date(date.getFullYear(), date.getMonth()+1, 0).getDate()
+    let tempDate = new Date(date)
+    let monthLength = new Date(tempDate.getFullYear(), tempDate.getMonth()+1, 0).getDate()
+    let res = []
+    for (let i = 1; i <= monthLength; i++){
+        res.push(new Date(tempDate.getFullYear(), tempDate.getMonth(), i).getTime())
+    }
+    res.unshift('')
+    return res
 }
 
 function generateCalendarData(unparsedData, days) {
     let fullCalendar = []
     let visibleRows = [{}]
-    let calDays = generateCalendarRow([...Array(days + 1).keys()], days)
-    calDays[0] = ''
-    fullCalendar.push(calDays)
     for(let i in unparsedData){
         fullCalendar.push(generateCalendarRow(unparsedData[i], days))
         visibleRows.push({row: i})
     }
-    console.log(fullCalendar);
+    fullCalendar.unshift(days)
     return {'visibleRows': visibleRows, 'displayData': fullCalendar}
 }
 
-function generateWeekData(unparsedData, curDate){
-    let weekStart = getWeekStart(curDate).getDate()
-    let weekEnd = weekStart + 6
-    let calDays = generateCalendarRow(Array.from({length: 8}, (_, i) => i + weekStart - 1), 7)
-    calDays[0] = ''
-    return generateNonEmptyList(unparsedData, weekStart, weekEnd, calDays);
-}
-
-function generateDayData(unparsedData, curDate){
-    let day = curDate.getDate()
-    let calDays = generateCalendarRow(Array.from({length: 2}, (_, i) => i + day - 1), 1)
-    calDays[0] = ''
-    return generateNonEmptyList(unparsedData, day, day, calDays);
-}
-
 function generateCalendarRow(unparsedRow, days) {
-    let calendarRow = [...Array(days + 1)]
     let result = {}
-    for(let i in calendarRow){
-        result[i] = unparsedRow[i] !== undefined ? unparsedRow[i] : ''
+    for(let [key, value] of days.entries()){
+        result[key] = unparsedRow[new Date(value).toLocaleDateString()] !== undefined ? unparsedRow[new Date(value).toLocaleDateString()] : ''
     }
+    result[0] = unparsedRow[0]
     return result
-}
-
-function generateNonEmptyList(unparsedRow, start, end, calDays){
-    let result = []
-    result.push(calDays)
-    let visibleRows = [{}]
-    let include = 0
-    unparsedRow.map((item, index) => {
-        include = 0
-        for(let i = start; i <= end; i++){
-            if(validWeekDayStatus.includes(item[i])){
-                include = 1
-            }
-        }
-        if(include === 1) {
-            result.push({})
-            let lastItem = result.length - 1
-            result[lastItem][0] = item[0]
-            for(let i = start; i <= end; i++){
-                result[lastItem][i] = item[i]
-            }
-            visibleRows.push({row: index})
-        }
-    })
-    console.log(result);
-    return {'visibleRows': visibleRows, 'displayData': result}
 }
